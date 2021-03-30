bed51c2796f11781440632c5ea82bafe32b27e39
Maria Elena Contini <mariaelenacontini@MBP-di-Maria-2.station>
2021-03-30 20:15:10 +0200

    ho aggiunto commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import {Hero} from './hero';
+
+export const HEROES : Hero[]=[
+    {id:11 , name: 'Superman'},
+    {id :12 , name:'Batman'},
+    {id:13 , name: 'Spiderman'},
+    {id:14 , name:'Hulk'},
+    {id:15, name:'Ironman'}
+];
